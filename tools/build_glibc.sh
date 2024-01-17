8ea9c2a6a84604fda38ab7ab418915dd5e7cf1f0
Martin Mauersics <revision@maus-games.at>
2024-01-17 14:08:08 +0100

    updated glibc to v2.28

--- tools/build_glibc.sh
+++ tools/build_glibc.sh
@@ -7,10 +7,10 @@ _PREFIX_="$_PATH_/../output/glibc"
 export CFLAGS="-Os -msse3"
 
 cd "$_PATH_/../modules/glibc"
-rm -r build
+#rm -r build
 mkdir build
 cd build
 ../configure --prefix=$_PREFIX_ --without-selinux --disable-werror --disable-timezone-tools
 make -j 10
 make install
-make clean
+#make clean
