f8bc65817370fc09e3975cfc4f823b1bca899533
Martin Mauersics <revision@maus-games.at>
2025-01-22 20:54:05 +0100

    updated all build scripts (for SDL3)

--- tools/process_linux.sh
+++ tools/process_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-patchelf --replace-needed libSDL2-2.0.so libSDL2.so libSDL2_ttf.so
-patchelf --replace-needed libSDL2-2.0.so libSDL2.so libSDL2_image.so
+#patchelf --replace-needed libSDL2-2.0.so libSDL2.so libSDL2_ttf.so
+#patchelf --replace-needed libSDL2-2.0.so libSDL2.so libSDL2_image.so
 
 ../../CoreEngine/tools/scripts/strip_soname.sh  "$PWD"
 ../../CoreEngine/tools/scripts/strip_symbols.sh "$PWD"
--- tools/process_macos.sh
+++ tools/process_macos.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-strip -S -x libSDL2.dylib
-strip -S -x libSDL2_ttf.dylib
-strip -S -x libSDL2_image.dylib
+strip -S -x libSDL3.dylib
+strip -S -x libSDL3_ttf.dylib
+strip -S -x libSDL3_image.dylib
 strip -S -x libopenal.dylib
 strip -S -x libzstd.dylib
 strip -S -x libopusfile.dylib
 
-install_name_tool -id @rpath/libSDL2.dylib       libSDL2.dylib
-install_name_tool -id @rpath/libSDL2_ttf.dylib   libSDL2_ttf.dylib
-install_name_tool -id @rpath/libSDL2_image.dylib libSDL2_image.dylib
+#install_name_tool -id @rpath/libSDL2.dylib       libSDL2.dylib
+#install_name_tool -id @rpath/libSDL2_ttf.dylib   libSDL2_ttf.dylib
+#install_name_tool -id @rpath/libSDL2_image.dylib libSDL2_image.dylib
 
-install_name_tool -change @rpath/libSDL2-2.0.dylib @rpath/libSDL2.dylib libSDL2_ttf.dylib
-install_name_tool -change @rpath/libSDL2-2.0.dylib @rpath/libSDL2.dylib libSDL2_image.dylib
+#install_name_tool -change @rpath/libSDL2-2.0.dylib @rpath/libSDL2.dylib libSDL2_ttf.dylib
+#install_name_tool -change @rpath/libSDL2-2.0.dylib @rpath/libSDL2.dylib libSDL2_image.dylib
