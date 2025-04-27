da0ccda23d2815f37c81b2970eea0aad18f906ff
Martin Mauersics <revision@maus-games.at>
2025-04-27 16:05:04 +0200

    changed default folder naming convention from PascalCase to dash-case (as this is the naming convention for all repositories)

--- tools/process_linux.sh
+++ tools/process_linux.sh
@@ -3,5 +3,5 @@
 #patchelf --replace-needed libSDL2-2.0.so libSDL2.so libSDL2_ttf.so
 #patchelf --replace-needed libSDL2-2.0.so libSDL2.so libSDL2_image.so
 
-../../CoreEngine/tools/scripts/strip_soname.sh  "$PWD"
-../../CoreEngine/tools/scripts/strip_symbols.sh "$PWD"
+../../core-engine/tools/scripts/strip_soname.sh  "$PWD"
+../../core-engine/tools/scripts/strip_symbols.sh "$PWD"
